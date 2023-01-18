96431f982519648135329a6b008923187b1c9d39
JustinPedley <jpedley24@gsb.columbia.edu>
2023-01-18 16:26:36 -0500

    Changes from 1/18 class

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from myapp import support_functions
+from myapp.models import Currency
+
 
 # Create your views here.
 
@@ -7,3 +10,27 @@ def home(request):
     import datetime
     data["time"] =datetime.date.today()
     return render(request, "home.html", context=data)
+
+def maintenance(request):
+    data = dict()
+    choice = "NONE"
+    try:
+       choice = request.GET["selection"]
+       if choice == "currencies":
+           support_functions.add_currencies(support_functions.get_currency_list())
+       c_list = Currency.objects.all()
+       #print("Got c_list", len(c_list))
+       data['currencies'] = c_list
+       #return HttpResponseRedirect(reverse('currencies'))
+
+    except:
+        pass
+    print(choice)
+
+    return render(request,"maintenance.html",context=data)
+
+def view_currencies(request):
+    data = dict()
+    c_list = Currency.objects.all()
+    data['currencies'] = c_list
+    return render(request,'currencies.html',context=data)
